1e18e9c6c9e9c5580767fb20fe6d8b6c861e40e9
Harri Feng <harrifeng@gmail.com>
2012-11-05 15:59:36 +0800

    add python-mode

---
+++ mylisps/pythonmode-setting.el
@@ -0,0 +1,5 @@
+(provide 'pythonmode-setting)
+
+(autoload 'python-mode "python-mode" "Python Mode." t)
+(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+(add-to-list 'interpreter-mode-alist '("python" . python-mode))
