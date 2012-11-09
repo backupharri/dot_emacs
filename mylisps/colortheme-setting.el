e1fe78fabc4e95f6ab3c6185d5ed6f793874d467
Harri Feng <harrifeng@gmail.com>
2012-11-09 21:11:19 +0800

    all in one colortheme folder

--- mylisps/colortheme-setting.el
+++ mylisps/colortheme-setting.el
@@ -1,14 +1,10 @@
 (provide 'colortheme-setting)
 
 (add-to-list 'custom-theme-load-path (concat their-lisps-path
-					     "colortheme-solarized"))
-
-(add-to-list 'custom-theme-load-path (concat their-lisps-path
-					     "colortheme-tomorrow"))
+					     "colortheme"))
 
 
 ;(load-theme 'solarized-dark t)
-
 (load-theme 'tomorrow-night-eighties t)
 
 
