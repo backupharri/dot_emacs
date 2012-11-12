a1099df1b37075299fe0e7098e5860005f6f240c
Harri Feng <harrifeng@gmail.com>
2012-11-12 20:02:16 +0800

    unified the behavior of eshell and shell

--- mylisps/shell-setting.el
+++ mylisps/shell-setting.el
@@ -1,10 +1,9 @@
+(provide 'shell-setting)
 
 ;; run mulitiple shell 
 (defun shell-mode-auto-rename-buffer (text)
-  (if (or
-       (eq major-mode 'shell-mode)
-       (eq major-mode 'eshell-mode))
-      (rename-buffer  (concat "shell: " default-directory) t)))
+  (if (eq major-mode 'shell-mode)
+      (rename-buffer  (concat "*Shell: " (concat default-directory "*")) t)))
 (add-hook 'comint-output-filter-functions 'shell-mode-auto-rename-buffer)
 
 ;; run mulitiple eshell 
@@ -15,8 +14,3 @@
 (add-hook 'eshell-directory-change-hook
     (lambda ()
         (rename-buffer (concat "*EShell: " (concat default-directory "*")) t)))
-
-
-
-(provide 'shell-setting)
-
