7dff0a791354402813da73381ef0e112ead93849
unknown <harrifeng>
2012-11-15 22:56:02 +0800

    this coding setting is ok for windows

--- mylisps/basic-setting.el
+++ mylisps/basic-setting.el
@@ -66,8 +66,12 @@
 
 
 ;;UTF-8 Setting
-(setq current-language-environment "UTF-8")
-(setq locale-coding-system 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(prefer-coding-system 'utf-8)
+(set-language-environment 'Chinese-GB)
+(set-keyboard-coding-system 'euc-cn)
+(set-clipboard-coding-system 'euc-cn)
+(set-terminal-coding-system 'euc-cn)
+(set-buffer-file-coding-system 'euc-cn)
+(set-selection-coding-system 'euc-cn)
+(modify-coding-system-alist 'process "*" 'euc-cn)
+(setq default-process-coding-system '(euc-cn . euc-cn))
+(setq-default pathname-coding-system 'euc-cn)
