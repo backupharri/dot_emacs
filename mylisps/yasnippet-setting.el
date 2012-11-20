124e8314b03f1dcc03f4c3a4243b818b3b940f0e
Harri Feng <harrifeng@gmail.com>
2012-11-20 10:39:13 +0800

    yasnippet will not need mouse if multiple choices appears

--- mylisps/yasnippet-setting.el
+++ mylisps/yasnippet-setting.el
@@ -4,3 +4,6 @@
 (yas/initialize)
 (yas/load-directory (concat their-lisps-path
 			    "yasnippet/snippets/"))
+
+(setq mmm-global-mode 't)
+(setq yas/prompt-functions '(yas/dropdown-prompt yas/x-prompt))
