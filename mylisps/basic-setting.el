c9f98355de27a5d109ca43d616d993da35046276
Harri Feng <harrifeng@gmail.com>
2012-11-27 14:40:24 +0800

    add wang yin's auto expand setting, complete one whole line at first

--- mylisps/basic-setting.el
+++ mylisps/basic-setting.el
@@ -76,3 +76,33 @@
 (modify-coding-system-alist 'process "*" 'euc-cn)
 (setq default-process-coding-system '(euc-cn . euc-cn))
 (setq-default pathname-coding-system 'euc-cn)
+
+
+
+;;自动补全功能，从王垠的网站直接Copy过来的，引用一些他对此的说明
+;;你可以设置以下 hippie-expand 的补全方式。它是一个优先列表， hippie-expand 会优
+;;先使用表最前面的函数来补全这是说，首先使用当前的buffer补全，如果找不到，就到别的可见
+;;的窗口里寻找，如果还找不到，那么到所有打开的buffer去找，如果还那么到kill-ring
+;;里，到文件名，到简称列表里，到list, 当前使用的匹配方式会在 echo 区域显示。
+;;特别有意思的是 try-expand-line，它可以帮你补全整整一行文字。我很多时后有两行文字大致
+;;相同，只有几个字不一样，但是我懒得去拷贝粘贴以下。那么我就输入这行文字的前面几个字。然后
+;;多按几下 M-/ 就能得到那一行。
+(global-set-key [(meta ?/)] 'hippie-expand)
+(setq hippie-expand-try-functions-list
+      '(
+	;senator-try-expand-semantic ;优先调用了senator的分析结果-很慢-还是使用吧
+	try-expand-line ; 补全当前行
+	try-expand-line-all-buffers
+	try-expand-list ; 补全一个列表
+	try-expand-list-all-buffers
+	try-expand-dabbrev ; 搜索当前 buffer
+	try-expand-dabbrev-visible ; 搜索当前可见窗口
+	try-expand-dabbrev-all-buffers ; 搜索所有 buffer
+	try-expand-dabbrev-from-kill ; 从 kill-ring 中搜索
+	try-complete-file-name ; 文件名匹配
+	try-complete-file-name-partially ; 文件名部分匹配
+	try-complete-lisp-symbol ; 补全 lisp symbol
+	try-complete-lisp-symbol-partially ; 部分补全 elisp symbol
+	try-expand-whole-kill
+	)
+      )
