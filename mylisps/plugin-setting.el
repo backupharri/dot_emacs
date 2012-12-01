e83b136147c150c2bc2c438025b0282b9c28ef9b
MACAir <harrifeng@gmail.com>
2012-12-01 16:01:55 +0800

    continue to set the default mechod for tramp, unix-like is ssh, and windows is plink

--- mylisps/plugin-setting.el
+++ mylisps/plugin-setting.el
@@ -5,7 +5,12 @@
 (require 'tramp)
 ;; tramp setting password keep time
 (setq password-cache-expiry 6000)
-(set-default 'tramp-default-proxies-alist (quote ((nil "\\`root\\'" "/ssh:hfeng@%h:"))))
+
+(if (string-equal system-type "windows-nt")
+    (setq tramp-default-method "plink")
+  (setq tramp-default-method "ssh"))
+
+(set-default 'tramp-default-proxies-alist (quote ((nil "\\`root\\'" "/hfeng@%h:"))))
 
 ;;--------recentf------------------>>
 (recentf-mode 1)
